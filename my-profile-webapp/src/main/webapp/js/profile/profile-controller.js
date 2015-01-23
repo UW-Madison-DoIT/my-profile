304ab04df620a6e197bb196fbfcff72001575bed
Tim Levett <levett@wisc.edu>
2015-01-23 10:12:51 -0600

    add in mock basic information

--- my-profile-webapp/src/main/webapp/js/profile/profile-controller.js
+++ my-profile-webapp/src/main/webapp/js/profile/profile-controller.js
@@ -3,12 +3,20 @@
 (function() {
   var app = angular.module('portal.profile.controllers', []);
 
-  app.controller('ContactInformationController', ['$localStorage','$scope', 'contactService', function($localStorage, $scope, contactService) {
+  app.controller('ContactInformationController', ['$localStorage','$scope', 'profileService', function($localStorage, $scope, profileService) {
     $scope.contactInfo = [];
-    contactService.getContactInfo().then(function(result){
+    profileService.getContactInfo().then(function(result){
         $scope.contactInfo = result.data.contactInfo;
     });
     
   } ]);
+  
+  app.controller('BasicInformationController', ['$localStorage','$scope', 'profileService', function($localStorage, $scope, profileService) {
+      $scope.basicInfo = [];
+      profileService.getBasicInfo().then(function(result){
+          $scope.basicInfo = result.data.basicInfo;
+      });
+      
+    } ]);
 
 })();
--- my-profile-webapp/src/main/webapp/js/profile/profile-service.js
+++ my-profile-webapp/src/main/webapp/js/profile/profile-service.js
@@ -3,20 +3,31 @@
 (function() {
     var app = angular.module('portal.profile.service', []);
     
-    app.factory('contactService', function($http, miscService) {
+    app.factory('profileService', function($http, miscService) {
       var contactInfoPromise = $http.get('/profile/samples/contact-info.json');
+      var basicInfoPromise = $http.get('/profile/samples/basic-info.json');
     
       var getContactInfo = function() {
           return contactInfoPromise.success(
-                  function(data, status) { //success function
-                      return data.contactInfo;
-                  }).error(function(data, status) { // failure function
-                  miscService.redirectUser(status, "Get contact info");
-                });
-              }
+             function(data, status) { //success function
+                 return data.contactInfo;
+             }).error(function(data, status) { // failure function
+             miscService.redirectUser(status, "Get contact info");
+          });
+      }
+      
+      var getBasicInfo = function() {
+          return basicInfoPromise.success(
+             function(data, status) { //success function
+                 return data.basicInfo;
+             }).error(function(data, status) { // failure function
+             miscService.redirectUser(status, "Get basic info");
+          });
+      }
     
       return {
-        getContactInfo : getContactInfo
+        getContactInfo : getContactInfo,
+        getBasicInfo   : getBasicInfo
       }
     });
 })();
