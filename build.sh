c8f86f6e8539aef3c29f4a9ee3661a05864980fb
Tim Levett <levett@wisc.edu>
2015-01-26 16:01:08 -0600

    added build.sh

--- build.sh
+++ build.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
+
 if [ "$#" = "0" ]; then
  mvn -Djava.awt.headless=true clean install
 else 
  mvn -Djava.awt.headless=true $@
 fi
 
-rm -rf ~/tomcat/tomcat/webapps/profile.war ~/tomcat/tomcat/webapps/profile
-cp ./my-profile-webapp/target/profile.war ~/tomcat/tomcat/webapps
+if [ `echo $TC_HOME` != "" ]; then
 
+rm -rf $TC_HOME/webapps/profile.war $TC_HOME/webapps/profile
+cp ./my-profile-webapp/target/profile.war $TC_HOME/webapps
+
+echo "WAR delivered to TC"
+
+fi
+
+exit 0;
