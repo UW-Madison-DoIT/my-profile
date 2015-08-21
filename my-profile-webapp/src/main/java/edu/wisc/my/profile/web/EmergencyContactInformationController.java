f1a2a5d62926a3ba7172f8abe559e1ff57d4372b
Tim Levett <levett@wisc.edu>
2015-08-21 10:13:22 -0500

    remove hrs and isis integration impl, add in mock impl

--- my-profile-webapp/src/main/java/edu/wisc/my/profile/web/EmergencyContactInformationController.java
+++ my-profile-webapp/src/main/java/edu/wisc/my/profile/web/EmergencyContactInformationController.java
@@ -3,7 +3,7 @@ package edu.wisc.my.profile.web;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
